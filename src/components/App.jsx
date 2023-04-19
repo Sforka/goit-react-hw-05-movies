b1c98c6cceac50b3defc4ea18642bf2841dd1592
Sforka <warlock20115@gmail.com>
2023-04-19 21:45:02 +0300

    1
    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,6 +22,7 @@ export const App = () => {
             <Route path="/movies/:movieId/cast" element={<Cast />} />
             <Route path="/movies/:movieId/reviews" element={<Reviews />} />
           </Route>
+          <Route path="*" element={<Home />} />
         </Routes>
       </Suspense>
     </div>
